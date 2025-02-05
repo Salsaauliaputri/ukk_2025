d2443bebcca8a8609cc3041b5f93f06c7539a78e
RaflyEsa <dzidanxpplg2@gmail.com>
2025-02-05 11:05:54 +0700

    halaman login

---
+++ lib/login.dart
@@ -0,0 +1,160 @@
+import 'package:flutter/material.dart';
+
+class LOGIN extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff3a0606),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(16, 100, 16, 16),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Padding(
+                padding: EdgeInsets.fromLTRB(0, 16, 0, 0),
+                child: Text(
+                  "LOGIN",
+                  textAlign: TextAlign.start,
+                  overflow: TextOverflow.clip,
+                  style: TextStyle(
+                    fontWeight: FontWeight.w800,
+                    fontStyle: FontStyle.normal,
+                    fontSize: 30,
+                    color: Color(0xffffffff),
+                  ),
+                ),
+              ),
+                 Padding(
+                padding: EdgeInsets.fromLTRB(0, 8, 0, 0),
+                child: Text(
+                  "Silakan Login Terlebih Dahulu !",
+                  textAlign: TextAlign.center,
+                  overflow: TextOverflow.clip,
+                  style: TextStyle(
+                    fontWeight: FontWeight.w400,
+                    fontStyle: FontStyle.normal,
+                    fontSize: 14,
+                    color: Color(0xffededed),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(10, 40, 10, 10),
+                child: TextField(
+                  controller: TextEditingController(),
+                  obscureText: false,
+                  textAlign: TextAlign.start,
+                  maxLines: 1,
+                  style: TextStyle(
+                    fontWeight: FontWeight.w400,
+                    fontStyle: FontStyle.normal,
+                    fontSize: 14,
+                    color: Color(0xff000000),
+                  ),
+                  decoration: InputDecoration(
+                    disabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                      borderSide:
+                          BorderSide(color: Color(0xff3a57e8), width: 1),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                      borderSide:
+                          BorderSide(color: Color(0xff3a57e8), width: 1),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                      borderSide:
+                          BorderSide(color: Color(0xff3a57e8), width: 1),
+                    ),
+                    hintText: "Username",
+                    hintStyle: TextStyle(
+                      fontWeight: FontWeight.w400,
+                      fontStyle: FontStyle.normal,
+                      fontSize: 14,
+                      color: Color(0xff000000),
+                    ),
+                    filled: true,
+                    fillColor: Color(0xffffffff),
+                    isDense: false,
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+                    prefixIcon:
+                        Icon(Icons.person, color: Color(0xff212435), size: 24),
+                  ),
+                ),
+              ),
+               Padding(
+                padding: EdgeInsets.all(10),
+                child: TextField(
+                  controller: TextEditingController(),
+                  obscureText: false,
+                  textAlign: TextAlign.start,
+                  maxLines: 1,
+                  style: TextStyle(
+                    fontWeight: FontWeight.w400,
+                    fontStyle: FontStyle.normal,
+                    fontSize: 14,
+                    color: Color(0xff000000),
+                  ),
+                  decoration: InputDecoration(
+                    disabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                      borderSide:
+                          BorderSide(color: Color(0xff3a57e8), width: 1),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                      borderSide:
+                          BorderSide(color: Color(0xff3a57e8), width: 1),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                      borderSide:
+                          BorderSide(color: Color(0xff3a57e8), width: 1),
+                    ),
+                      hintText: "Password",
+                    hintStyle: TextStyle(
+                      fontWeight: FontWeight.w400,
+                      fontStyle: FontStyle.normal,
+                      fontSize: 14,
+                      color: Color(0xff000000),
+                    ),
+                    filled: true,
+                    fillColor: Color(0xffffffff),
+                    isDense: false,
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+                    prefixIcon: Icon(Icons.visibility,
+                        color: Color(0xff212435), size: 24),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(10, 16, 10, 10),
+                child: MaterialButton(
+                  onPressed: () {},
+                  color: Color(0xff8e7373),
+                  elevation: 0,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                    padding: EdgeInsets.all(16),
+                  child: Text(
+                    'LOGIN'
+                  ),
+                  textColor: Color(0xffffffff),
+                  height: 45,
+                  minWidth: MediaQuery.of(context).size.width,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+
+import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:ukk_2025/login.dart';
+
+Future<void> main() async {
+  await Supabase.initialize(
+    url: 'https://xmbnplpjxsngjupqtxim.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhtYm5wbHBqeHNuZ2p1cHF0eGltIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg3MTM1OTAsImV4cCI6MjA1NDI4OTU5MH0.m26BTDj8q1LNVYgzVAQJRsIftQILGvKQn7okk2LqPWQ',
+  );
+  runApp(MyApp());
+}
+        
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'LOGIN',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: LOGIN(),
+    );
+  }
+}
+
